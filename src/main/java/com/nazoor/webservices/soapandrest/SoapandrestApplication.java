8991fe466d0b797ed914f7b9781e6d322debd631
Mohomed Nazar Nazoor Ahamed <nazar.2016355@iit.ac.lk>
2020-12-06 11:04:24 +0530

    spring initializing

---
+++ src/main/java/com/nazoor/webservices/soapandrest/SoapandrestApplication.java
@@ -0,0 +1,13 @@
+package com.nazoor.webservices.soapandrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapandrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapandrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazoor/webservices/soapandrest/SoapandrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazoor.webservices.soapandrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapandrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
